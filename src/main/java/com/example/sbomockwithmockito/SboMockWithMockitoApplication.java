d5555de3845ba07d38517e0b9a7c17cab9c8c29f
gustavo-rethink <gustavo@re-think.consulting>
2019-06-10 20:43:50 +0100

    Create the project

---
+++ src/main/java/com/example/sbomockwithmockito/SboMockWithMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbomockwithmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SboMockWithMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SboMockWithMockitoApplication.class, args);
+    }
+
+}
