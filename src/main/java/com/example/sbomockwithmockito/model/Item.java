329e425d138bf9c9acbde13bb46c53866bff7fc9
Gustavo Gois <gustavo.gois@gmail.com>
2019-06-15 23:04:06 +0100

    Item Entity

--- src/main/java/com/example/sbomockwithmockito/model/Item.java
+++ src/main/java/com/example/sbomockwithmockito/model/Item.java
@@ -1,57 +1,57 @@
 package com.example.sbomockwithmockito.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class Item {
-    private int id;
-    private String name;
-    private int price;
-    private int quantity;
 
-    public Item(int id, String name, int price, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
+	@Id
+	private int id;
+	private String name;
+	private int price;
+	private int quantity;
 
-    public int getId() {
-        return id;
-    }
+	@Transient
+	private int value;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	protected Item() {
+		
+	}
+	
+	public Item(int id, String name, int price, int quantity) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getPrice() {
-        return price;
-    }
+	public int getPrice() {
+		return price;
+	}
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
+	public int getQuantity() {
+		return quantity;
+	}
 
-    public int getQuantity() {
-        return quantity;
-    }
+	public int getValue() {
+		return value;
+	}
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+	public void setValue(int value) {
+		this.value = value;
+	}
 
-    @Override
-    public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", quantity=" + quantity +
-                '}';
-    }
+	public String toString() {
+		return String.format("Item[%d, %s, %d, %d]", id, name, price, quantity);
+	}
 }
