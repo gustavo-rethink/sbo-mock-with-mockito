6a7a6404ac146544e2f164bc4cb52cf75af49aca
gustavo-rethink <gustavo@re-think.consulting>
2019-06-26 08:51:26 +0200

    Create configurations test

--- src/test/java/com/example/sbomockwithmockito/SboMockWithMockitoApplicationTests.java
+++ src/test/java/com/example/sbomockwithmockito/SboMockWithMockitoApplicationTests.java
@@ -3,10 +3,12 @@ package com.example.sbomockwithmockito;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:test-configuration.properties"})
 public class SboMockWithMockitoApplicationTests {
 
     @Test
